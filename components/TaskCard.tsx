b0899580cb1b768646f20629033596501681e397
lerato-s <sikhumbanal@gmail.com>
2026-08-11 20:23:05 +0200

    added overdue flagging

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -14,9 +14,12 @@ type Task = {
   };
   
   export default function TaskCard({ task, onArchive }: TaskCardProps) {
+
+    const isOverdue = new Date(task.due_date) < new Date() && task.status !== "Complete";
     return (
       <div className="p-4 rounded-xl border border-gray-200 shadow-sm mb-4">
         <h2 className="font-bold">{task.title}</h2>
+        {isOverdue && <p className="text-red-600 font-bold">Overdue!</p>}
         <p>{task.description}</p>
         <p>Due: {task.due_date}</p>
         <p>{task.topic}</p>
